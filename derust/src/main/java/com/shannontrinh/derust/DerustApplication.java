93cb229980cb6ff6fa03e3be5eca5fa4cf4755f0
Shannon Trinh <33528736+slinky102@users.noreply.github.com>
2025-06-06 15:16:15 -0700

    Created Springboot project using initializer

---
+++ derust/src/main/java/com/shannontrinh/derust/DerustApplication.java
@@ -0,0 +1,13 @@
+package com.shannontrinh.derust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DerustApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DerustApplication.class, args);
+	}
+
+}
---
+++ derust/src/test/java/com/shannontrinh/derust/DerustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shannontrinh.derust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DerustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
